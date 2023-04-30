9aeea97536c814866fb9c54963dbe9b82272d312
Baturpcu <batusalkim11@gmail.com>
2023-05-01 02:57:08 +0300

    update

---
+++ cryptoapiapp/src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+function App() {
+ 
+  return (
+    <div style={{fontFamily:"Avenir"}}>
+      <Router>
+        <Switch>
+
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
