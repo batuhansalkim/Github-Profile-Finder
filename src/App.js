35be71c7dbf54f02829bf8f179119bc427faf2d4
Baturpcu <batusalkim11@gmail.com>
2023-05-01 16:22:00 +0300

    update

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import { Home } from './pages/Home'
+
+function App() {
+  return (
+    <div>
+      <Register/>
+      <Login/>
+      <Home/>
+    </div>
+    
+  )
+}
+
+export default App
--- src/contexts/AuthContext.js
+++
@@ -1,3 +0,0 @@
-import React,{useContext,useState,useEffect} from "react";
-import {useHistory} from "react-router-dom";
-import {auth} from "../firebase";x
