fd9ea40f6578a4dc8c4e5e5ea0880481c19cd7a1
Baturpcu <batusalkim11@gmail.com>
2023-05-01 14:18:08 +0300

    update

--- cryptoapiapp/src/components/Login.js
+++ cryptoapiapp/src/components/Login.js
@@ -1,18 +1,24 @@
 import React from 'react'
 import {GoogleOutlined,FacebookOutlined} from "@ant-design/icons";
 import "../index.css";
+import firebase from "firebase/app";
+import {auth} from "../firebase";
+
 
 const  Login=()=> {
   return (
     <div id="login-page">
         <div id="login-card">
             <h2>Welcome to Unichat!</h2>
-            <div className='login-button google'>
+            <div className='login-button google' onClick={()=>auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}>
                 <GoogleOutlined/> Sign In With Google
-            </div><br /><br />
-            <div className='login-button facebook'>
+            </div>
+            <br /><br />
+
+            <div className='login-button facebook' onClick={()=>auth.signInWithRedirect(new firebase.auth.FacebookAuthProvider())}>
                 <FacebookOutlined/> Sign In With Facebook
             </div>
+            
         </div>
     </div>
   )
---
+++ cryptoapiapp/src/contexts/AuthContext.js
@@ -0,0 +1,3 @@
+import React,{useContext,useState,useEffect} from "react";
+import {useHistory} from "react-router-dom";
+import {auth} from "../firebase";
