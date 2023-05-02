3ceb8ab0a2dea3d21d44adf60880b0ed7c484711
Baturpcu <batusalkim11@gmail.com>
2023-05-02 15:10:04 +0300

    update

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react'
 import Register from './pages/Register'
 import Login from './pages/Login'
 import { Home } from './pages/Home'
+import "./style.scss";
 
 function App() {
   return (
     <div>
       <Register/>
-      <Login/>
-      <Home/>
+      
     </div>
     
   )
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -2,7 +2,20 @@ import React from 'react'
 
 function Register() {
   return (
-    <div>Register</div>
+    <div className='formContainer'>
+      <div className='formWrapper'>
+        <span className='logo'>Batu Chat App</span>
+        <span className='title'>Register</span>
+        <form>
+          <input type="text" placeholder='display name'/>
+          <input type="email" placeholder='email'/>
+          <input type="password" placeholder='password'/>
+          <input type="file" />
+          <button>Sign Up</button>
+        </form>
+        <p>You do have an account? Login</p>
+      </div>
+    </div>
   )
 }
 
