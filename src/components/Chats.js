aaa058ddfca17d2affbbbf7aa1b0a32713f4e85b
Baturpcu <batusalkim11@gmail.com>
2023-05-04 16:52:22 +0300

    update

---
+++ src/components/Chats.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Chats() {
+  return (
+    <div className='chats'>
+        <div className="userChat">
+            <img src="https://images.pexels.com/photos/16160789/pexels-photo-16160789.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+            <div className='userChatInfo'>
+                <span>Batu</span>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Chats
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from "../components/Navbar";
 import Search from "../components/Search";
+import Chats from './Chats';
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
         <Navbar/>
         <Search/>
+        <Chats/>
     </div>
   )
 }
