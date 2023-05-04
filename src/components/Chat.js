29df80201865c9d92fd06f34df82f758f02fd38f
Baturpcu <batusalkim11@gmail.com>
2023-05-04 18:59:58 +0300

    update

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import Cam from "../img/cam.png";
+import Add from "../img/add.png";
+import More from "../img/more.png";
 
 function Chat() {
   return (
-    <div className='chat'>Chat</div>
+    <div className='chat'>
+        <div className="chatInfo">
+            <span>Jane</span>
+            <div className='chatIcons'>
+                <img src={Cam} alt="" />
+                <img src={Add} alt="" />
+                <img src={More} alt="" />
+            </div>
+        </div>
+    </div>
   )
 }
 
---
+++ src/components/Messages.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Messages() {
+  return (
+    <div>Messages</div>
+  )
+}
+
+export default Messages
