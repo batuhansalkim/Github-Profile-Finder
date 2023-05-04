d71f5e66dd9f3bfa5e5d5d4f1afc50bb8e6bbb7e
Baturpcu <batusalkim11@gmail.com>
2023-05-04 17:42:20 +0300

    update

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -7,6 +7,21 @@ function Chats() {
             <img src="https://images.pexels.com/photos/16160789/pexels-photo-16160789.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
             <div className='userChatInfo'>
                 <span>Batu</span>
+                <p>Hello</p>
+            </div>
+        </div>
+        <div className="userChat">
+            <img src="https://images.pexels.com/photos/16160789/pexels-photo-16160789.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+            <div className='userChatInfo'>
+                <span>Batu</span>
+                <p>Hello</p>
+            </div>
+        </div>
+        <div className="userChat">
+            <img src="https://images.pexels.com/photos/16160789/pexels-photo-16160789.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+            <div className='userChatInfo'>
+                <span>Batu</span>
+                <p>Hello</p>
             </div>
         </div>
     </div>
--- src/components/Search.js
+++ src/components/Search.js
@@ -6,7 +6,12 @@ function Search() {
         <div className="searchForm">
             <input type="text" placeholder='find a user'/>
         </div>
-        
+        {/* <div className="userChat">
+              <img src="https://images.pexels.com/photos/16160789/pexels-photo-16160789.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+              <div className='userChatInfo'>
+                <span>Batu</span>
+              </div>
+        </div> */}
         
     </div>
   )
