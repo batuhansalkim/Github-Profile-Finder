5687b12609d87e47076b8b69e51db2322fef8209
Baturpcu <batusalkim11@gmail.com>
2023-05-04 21:17:13 +0300

    update

--- src/components/Message.js
+++ src/components/Message.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Message() {
   return (
-    <div className='message'>
+    <div className='message owner'>
       <div className="messageInfo">
         <img src="https://images.pexels.com/photos/16354624/pexels-photo-16354624.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="" />
         <span>Batu</span>
@@ -10,7 +10,7 @@ function Message() {
 
       <div className='messageContent'>
         <p>hello</p>
-        {/* <img src="https://images.pexels.com/photos/16540903/pexels-photo-16540903.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" /> */}
+        <img src="https://images.pexels.com/photos/16540903/pexels-photo-16540903.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
       </div>
     </div>
   )
