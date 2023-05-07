919d1854b24d82645ef2216cb3335687e72a56a7
Baturpcu <batusalkim11@gmail.com>
2023-05-07 23:32:24 +0300

    update

--- src/App.js
+++ src/App.js
@@ -3,14 +3,29 @@ import Register from './pages/Register'
 import Login from './pages/Login'
 import { Home } from './pages/Home'
 import "./style.scss";
-import {BrowserRouter,Routes,Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, } from "react-router-dom";
+
 
 function App() {
   return (
-    <div>
-      <Register/>
-      
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Route index element={<Home/>}/>
+          <Route path="login" element={<Login/>}/>
+          <Route path="register" element={<Register/>}/>
+        </Route>
+      </Switch>
+    </Router>
+    // <Router>
+    //   <Switch>
+    //   <Route path="/">
+    //     <Route index element={<Home/>}/>
+    //     <Route path="login" element={<Login/>}/>
+    //     <Route path="register" element={<Register/>}/>
+    //   </Route>
+    //   </Switch>
+    // </Router>
     
   )
 }
