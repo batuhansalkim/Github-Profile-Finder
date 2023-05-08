a94bedadfd0c3c5c9636418df66a212b2d9f675c
Baturpcu <batusalkim11@gmail.com>
2023-05-08 05:00:43 +0300

    s

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Register from './pages/Register'
 import Login from './pages/Login'
 import { Home } from './pages/Home'
 import "./style.scss";
-import { BrowserRouter,Routes, Route, } from "react-router-dom";
+import { BrowserRouter,Routes, Route,Navigate } from "react-router-dom";
+import { AuthContext } from './context/AuthContext';
 
 
 function App() {
+
+  const {currentUser} = useContext(AuthContext);
+
+  const ProtectedRoute = ({children})=>{
+    if(!currentUser){
+      return<Navigate to="/login"/>
+    }
+  }
+
   return (
     <BrowserRouter>
       <Routes>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { signOut } from "firebase/auth";
+import { auth } from '../firebase';
 
 function Navbar() {
   return (
@@ -7,7 +9,7 @@ function Navbar() {
         <div className="user">
               <img src="https://images.pexels.com/photos/16160789/pexels-photo-16160789.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
             <span>Batu</span>
-            <button>Logout</button>
+            <button onClick={()=>signOut(auth)}>Logout</button>
         </div>
     </div>
   )
